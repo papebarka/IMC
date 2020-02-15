6db2f6565e898443b374fd445d28a1dd2a54d0d3
Pape Barka <papebarka@gmail.com>
2020-02-15 11:23:59 +0000

    Conversion table implemented

---
+++ main.rb
@@ -0,0 +1,15 @@
+def lbs_to_kg(weight)
+    new_weight = weight * 0.45359237
+end
+
+def kg_to_lbs(weight)
+    new_weight = weight / 0.45359237
+end
+
+def ft_to_cm(measure)
+    new_measure = measure * 30.48
+end
+
+def cm_to_ft(measure)
+    new_measure = measure / 30.48
+end
